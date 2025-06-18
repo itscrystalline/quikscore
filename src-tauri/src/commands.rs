949a1525c634113a6722370415e233860ce8bb2b
itscrystalline <pvpthadgaming@gmail.com>
2025-06-18 17:12:38 +0700

    fix: appstate changes lost in merge

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -29,7 +29,7 @@ pub fn upload_key_image(app: AppHandle) {
 pub fn clear_key_image(app: AppHandle) {
     let mutex = app.state::<StateMutex>();
     let mut state = mutex.lock().unwrap();
-    if let AppState::WithKeyImage { .. } = *state {
+    if let AppState::WithKey { .. } = *state {
         *state = AppState::Init;
         signal!(app, SignalKeys::KeyImage, "");
         signal!(app, SignalKeys::KeyStatus, "");
@@ -55,8 +55,14 @@ pub fn upload_sheet_images(app: AppHandle) {
 pub fn clear_sheet_images(app: AppHandle) {
     let mutex = app.state::<StateMutex>();
     let mut state = mutex.lock().unwrap();
-    if let AppState::WithKeyAndSheets { ref key, .. } = *state {
-        *state = AppState::WithKeyImage { key: key.clone() };
+    if let AppState::WithKeyAndSheets {
+        key, ref key_image, ..
+    } = *state
+    {
+        *state = AppState::WithKey {
+            key_image: key_image.clone(),
+            key,
+        };
         signal!(app, SignalKeys::SheetImages, Vec::<String>::new());
         signal!(app, SignalKeys::SheetStatus, "");
     }
