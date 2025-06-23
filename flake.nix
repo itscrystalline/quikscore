4c6efac66d24d0378698467a8e64fc66095220c4
itscrystalline <pvpthadgaming@gmail.com>
2025-06-23 11:23:18 +0700

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Flake for quikscore";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        inherit (pkgs) lib stdenv fetchYarnDeps;
+        inherit (pkgs.rustPlatform) buildRustPackage;
+        package = buildRustPackage (finalAttrs: {
+          pname = "quikscore";
+          version = "0.1.0";
+
+          src = ./.;
+
+          yarnOfflineCache = fetchYarnDeps {
+            yarnLock = finalAttrs.src + "/yarn.lock";
+            hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          };
+
+          nativeBuildInputs = with pkgs; [
+            yarnConfigHook
+            yarnBuildHook
+            yarnInstallHook
+            nodejs
+            cargo-tauri.hook
+            pkg-config
+          ];
+
+          cargoRoot = "src-tauri";
+
+          buildAndTestSubdir = "src-tauri";
+
+          buildInputs = lib.optionals stdenv.hostPlatform.isLinux (with pkgs; [
+            glib
+            gtk3
+            openssl
+            webkitgtk_4_1
+          ]);
+        });
+      in {
+        packages = {
+          quikscore = package;
+          default = package;
+        };
+      }
+    );
+}
