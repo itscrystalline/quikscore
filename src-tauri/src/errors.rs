08ddc3a1685c69e94a1b40be1cd00feb7bf94477
Sirijert Sutisukon <sirijertsutisukon@Sirijerts-MacBook-Pro.local>
2025-06-24 13:02:41 +0700

    Fix # 02 Redundant openCV::error

--- src-tauri/src/errors.rs
+++ src-tauri/src/errors.rs
@@ -20,4 +20,6 @@ pub enum SheetError {
     OpenCvError(#[from] opencv::Error),
     #[error("Detected less than 5 answers (this should not happen)")]
     TooLittleAnswers,
+    #[error("Tesseract Error: {0}")]
+    TesseractError(#[from] tesseract_rs::TesseractError),
 }
