46baab41f5643533130ca9f415b6621165eaa069
itscrystalline <pvpthadgaming@gmail.com>
2025-07-02 13:31:45 +0700

    feat: added stacked progressbar

---
+++ src/assets/StackedProgressBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+type ProgressBarProps = | {
+  type: "progress"
+  progressA: number,
+  progressB: number,
+  max: number
+} | { type: "indeterminate" };
+
+const props = defineProps<ProgressBarProps>();
+</script>
+
+<template>
+  <div class="progressbar">
+    <div class="layer-a" v-if="props.type == 'progress'">
+    </div>
+    <div class="layer-b" v-if="props.type == 'progress'">
+    </div>
+    <div class="indeterminate" v-else-if="props.type == 'indeterminate'">
+    </div>
+  </div>
+</template>
+
+<style></style>
