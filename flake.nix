28d80bd256f97bdedca93b55fa64c47b66bccb1a
Oracle Public Cloud User <opc@instance-20231102-1125.subnet11012121.vcn11012121.oraclevcn.com>
2025-07-24 09:24:18 +0700

    chore: dont build avx and non avx seperately at all

--- flake.nix
+++ flake.nix
@@ -37,123 +37,121 @@
           exec "`dirname $0`/.quikscore-wrapped" "$@"
         '';
 
-        package = avx:
-          nightlyPlatform.buildRustPackage (finalAttrs: {
-            pname = "quikscore";
-            version = "0.1.0";
+        package = nightlyPlatform.buildRustPackage (finalAttrs: {
+          pname = "quikscore";
+          version = "0.1.0";
 
-            src = ./.;
+          src = ./.;
 
-            yarnOfflineCache = fetchYarnDeps {
-              yarnLock = finalAttrs.src + "/yarn.lock";
-              hash = "sha256-bnSADccuQUUuvQE7TnQgEujfUGJF0BXdWzH4ZKxy+OM=";
-            };
+          yarnOfflineCache = fetchYarnDeps {
+            yarnLock = finalAttrs.src + "/yarn.lock";
+            hash = "sha256-bnSADccuQUUuvQE7TnQgEujfUGJF0BXdWzH4ZKxy+OM=";
+          };
 
-            nativeBuildInputs = with pkgs; [
-              yarnConfigHook
-              cargo-tauri.hook
-              nightlyPlatform.bindgenHook
+          nativeBuildInputs = with pkgs; [
+            yarnConfigHook
+            cargo-tauri.hook
+            nightlyPlatform.bindgenHook
 
-              nodejs
-              pkg-config
-              clang
-              patchelf
-              makeWrapper
-            ];
+            nodejs
+            pkg-config
+            clang
+            patchelf
+            makeWrapper
+          ];
 
-            env = {
-              RUSTFLAGS = "-Z threads=8";
-              OPENCV_LINK_PATHS = "${pkgs.opencv}/lib";
-              OPENCV_LINK_LIBS = "opencv_core,opencv_imgproc,opencv_imgcodecs,png";
-              OPENCV_INCLUDE_PATHS = "+${pkgs.opencv}/include";
-            };
+          env = {
+            RUSTFLAGS = "-Z threads=8";
+            OPENCV_LINK_PATHS = "${pkgs.opencv}/lib";
+            OPENCV_LINK_LIBS = "opencv_core,opencv_imgproc,opencv_imgcodecs,png";
+            OPENCV_INCLUDE_PATHS = "+${pkgs.opencv}/include";
+          };
 
-            cargoRoot = "src-tauri";
-            cargoLock = {
-              lockFile = finalAttrs.src + "/${finalAttrs.cargoRoot}/Cargo.lock";
-            };
-            cargoBuildFeatures = lib.optionals avx ["avx512"];
+          cargoRoot = "src-tauri";
+          cargoLock = {
+            lockFile = finalAttrs.src + "/${finalAttrs.cargoRoot}/Cargo.lock";
+          };
+          cargoBuildFeatures = ["avx512"];
 
-            buildAndTestSubdir = "src-tauri";
-            # useNextest = true;
-            doCheck = false;
+          buildAndTestSubdir = "src-tauri";
+          # useNextest = true;
+          doCheck = false;
 
-            buildInputs = with pkgs; (lib.optionals stdenv.hostPlatform.isLinux [
-                glib
-                gtk3
-                openssl
-                webkitgtk_4_1
-              ]
-              ++ [
-                opencv
-                libpng
-                openssl
-              ]);
+          buildInputs = with pkgs; (lib.optionals stdenv.hostPlatform.isLinux [
+              glib
+              gtk3
+              openssl
+              webkitgtk_4_1
+            ]
+            ++ [
+              opencv
+              libpng
+              openssl
+            ]);
 
-            postFixup =
-              if stdenv.hostPlatform.isLinux
-              then ''
-                echo Patching ELF loader to a non-nix path...
-                patchelf --set-interpreter ${loaderPath} $out/bin/quikscore
+          postFixup =
+            if stdenv.hostPlatform.isLinux
+            then ''
+              echo Patching ELF loader to a non-nix path...
+              patchelf --set-interpreter ${loaderPath} $out/bin/quikscore
 
-                echo Adding wrapper script...
-                mv $out/bin/quikscore $out/bin/.quikscore-wrapped
-                cp ${libPathPatchScript} $out/bin/quikscore
-                chmod +x $out/bin/quikscore
-              ''
-              else ''
-                echo "Rewriting lib path to system libc++"
-                binary="$out/Applications/quikscore.app/Contents/MacOS/quikscore"
+              echo Adding wrapper script...
+              mv $out/bin/quikscore $out/bin/.quikscore-wrapped
+              cp ${libPathPatchScript} $out/bin/quikscore
+              chmod +x $out/bin/quikscore
+            ''
+            else ''
+              echo "Rewriting lib path to system libc++"
+              binary="$out/Applications/quikscore.app/Contents/MacOS/quikscore"
 
-                echo "Before:"
-                otool -L "$binary"
+              echo "Before:"
+              otool -L "$binary"
 
-                install_name_tool -change \
-                  ${pkgs.libcxx}/lib/libc++.1.0.dylib \
-                  /usr/lib/libc++.1.dylib \
-                  "$binary"
+              install_name_tool -change \
+                ${pkgs.libcxx}/lib/libc++.1.0.dylib \
+                /usr/lib/libc++.1.dylib \
+                "$binary"
 
-                echo "After:"
-                otool -L "$binary"
-              '';
+              echo "After:"
+              otool -L "$binary"
+            '';
 
-            postInstall =
-              if stdenv.hostPlatform.isLinux
-              then ''
-                echo "Bundling additional libraries (OpenCV, OpenBLAS, OpenEXR)"
-                mkdir -p $out/lib
-                for lib in core imgproc imgcodecs ; do
-                  cp "${pkgs.opencv}/lib/libopencv_$lib.so.411" "$out/lib/"
-                done
-                cp "${pkgs.openblas}/lib/libopenblas.so.0" "$out/lib/"
-                cp -a "${pkgs.openexr.out}/lib/." "$out/lib/"
-              ''
-              else ''
-                echo "Bundling additional dylibs (OpenCV, libpng, libiconv)"
-                binary="$out/Applications/quikscore.app/Contents/MacOS/quikscore"
-                frameworks="$out/Applications/quikscore.app/Contents/Frameworks"
+          postInstall =
+            if stdenv.hostPlatform.isLinux
+            then ''
+              echo "Bundling additional libraries (OpenCV, OpenBLAS, OpenEXR)"
+              mkdir -p $out/lib
+              for lib in core imgproc imgcodecs ; do
+                cp "${pkgs.opencv}/lib/libopencv_$lib.so.411" "$out/lib/"
+              done
+              cp "${pkgs.openblas}/lib/libopenblas.so.0" "$out/lib/"
+              cp -a "${pkgs.openexr.out}/lib/." "$out/lib/"
+            ''
+            else ''
+              echo "Bundling additional dylibs (OpenCV, libpng, libiconv)"
+              binary="$out/Applications/quikscore.app/Contents/MacOS/quikscore"
+              frameworks="$out/Applications/quikscore.app/Contents/Frameworks"
 
-                mkdir -p "$frameworks"
-                for lib in core imgproc imgcodecs ; do
-                  cp "${pkgs.opencv}/lib/libopencv_$lib.dylib" $frameworks/
-                done
-                cp "${pkgs.libpng}/lib/libpng16.16.dylib" $frameworks/
-                cp "${pkgs.libiconv}/lib/libiconv.2.dylib" $frameworks/
+              mkdir -p "$frameworks"
+              for lib in core imgproc imgcodecs ; do
+                cp "${pkgs.opencv}/lib/libopencv_$lib.dylib" $frameworks/
+              done
+              cp "${pkgs.libpng}/lib/libpng16.16.dylib" $frameworks/
+              cp "${pkgs.libiconv}/lib/libiconv.2.dylib" $frameworks/
 
-                for dylib in $out/Applications/quikscore.app/Contents/Frameworks/*.dylib; do
-                  install_name_tool -id "@loader_path/../Frameworks/$(basename "$dylib")" "$dylib"
-                done
+              for dylib in $out/Applications/quikscore.app/Contents/Frameworks/*.dylib; do
+                install_name_tool -id "@loader_path/../Frameworks/$(basename "$dylib")" "$dylib"
+              done
 
-                for dep in $(otool -L $out/Applications/quikscore.app/Contents/MacOS/quikscore | grep "/nix/store" | awk '{print $1}'); do
-                  name=$(basename "$dep")
-                  install_name_tool -change "$dep" "@loader_path/../Frameworks/$name" $out/Applications/quikscore.app/Contents/MacOS/quikscore
-                done
-              '';
-          });
+              for dep in $(otool -L $out/Applications/quikscore.app/Contents/MacOS/quikscore | grep "/nix/store" | awk '{print $1}'); do
+                name=$(basename "$dep")
+                install_name_tool -change "$dep" "@loader_path/../Frameworks/$name" $out/Applications/quikscore.app/Contents/MacOS/quikscore
+              done
+            '';
+        });
       in {
         packages = rec {
-          quikscore = package true;
-          quikscore-compat = package false;
+          quikscore = package;
           default = quikscore;
         };
       }
