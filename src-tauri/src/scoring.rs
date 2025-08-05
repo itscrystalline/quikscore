009db17d54faf2ac7d9d5c43935ba4ee52f87ab9
Punnawithsut <punnawith05@gmail.com>
2025-08-05 15:48:13 +0700

    handled double answer case

--- src-tauri/src/scoring.rs
+++ src-tauri/src/scoring.rs
@@ -58,17 +58,10 @@ pub enum CheckedAnswer {
     Incorrect,
     Missing,
     NotCounted,
-    Doubled,
 }
 
 impl Answer {
     pub fn check_with(curr: Option<Answer>, key: Option<Answer>) -> CheckedAnswer {
-        if let Some(curr) = curr {
-            if curr.number == 255 {
-                return CheckedAnswer::Doubled;
-            }
-        }   
-        
         match (curr, key) {
             (Some(curr), Some(key)) => {
                 if curr == key {
@@ -84,7 +77,6 @@ impl Answer {
     pub fn from_bubbles_vec(vec: Vec<u8>) -> Option<Answer> {
         let mut num_type: Option<NumberType> = None;
         let mut num: Option<u8> = None;
-        let mut doubled = false;
 
         for idx in vec {
             if idx < 3 {
@@ -96,23 +88,17 @@ impl Answer {
                         _ => unreachable!(),
                     });
                 } else {
-                    doubled = true;
-                    break;
+                    return None;
                 }
             } else {
                 if num.is_none() {
                     num = Some(idx - 3);
                 } else {
-                    doubled = true;
-                    break;
+                    return None;
                 }
             }
         }
-        if doubled {
-            num = Some(255);
-        }
-
-        Some(Answer { num_type: num_type, number: num.unwrap() })
+        Some(Answer { num_type, number: num? })
     }
 }
 
@@ -128,7 +114,7 @@ impl QuestionGroup {
         let weight_arr = [weights.A, weights.B, weights.C, weights.D, weights.E];
         arr.iter_mut()
             .zip(weight_arr.iter())
-            .for_each(|(c, weight)| {
+            .for_each(|(cpe, weight)| {
                 if let CheckedAnswer::Correct(score) = c {
                     _ = score.insert(*weight);
                 }
@@ -149,7 +135,6 @@ impl CheckedQuestionGroup {
                 CheckedAnswer::Correct(_) => correct += 1,
                 CheckedAnswer::Missing => not_answered += 1,
                 CheckedAnswer::NotCounted => (),
-                CheckedAnswer::Doubled => incorrect += 1,
             }
         }
         (correct, incorrect, not_answered)
