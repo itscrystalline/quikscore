3f3f0b19b0044c12dba05a04c686fc8d4c9a87d0
Punnawithsut <punnawith05@gmail.com>
2025-09-08 15:59:35 +0700

    fix: server.js to handle login function:

--- server/server.js
+++ server/server.js
@@ -4,15 +4,23 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = 5000;
-const JWT_SECRET = process.env.JWT_SECRET;
+
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/getSecret", (req, res) => {
-    res.json({success: true, jwt: JWT_SECRET});
-}); 
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    if(username == ADMIN_USERNAME && password == ADMIN_PASSWORD) {
+        res.json({success: true, message: "SUCCESS!"});
+        return;
+    }
+    res.json({success: false, message: "Invalid Credentials, please contact admin"});
+})
 
 app.listen(PORT, () => {
     console.log(`Server is currently listening to port ${PORT}`);
