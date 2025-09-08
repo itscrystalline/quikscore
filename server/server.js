e796e1533c0835fde991fffa85c35641c140542d
Punnawithsut <punnawith05@gmail.com>
2025-09-08 15:41:58 +0700

    add: getSecret api end point

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT = 5000 || process.env.PORT;
+const PORT = 5000;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/getSecret", (req, res) => {
+    res.json({success: true, jwt: JWT_SECRET});
+}); 
 
 app.listen(PORT, () => {
     console.log(`Server is currently listening to port ${PORT}`);
